1fd31f5d319e5157e97feba174552b993afe66dd
Victor Sanchez <thespielmaster@macbookpro-1.home>
2025-10-13 11:28:04 +0200

    add direction getter to option

--- src/option.py
+++ src/option.py
@@ -2,11 +2,12 @@ from abc import ABC, abstractmethod
 import dataclasses
 
 from src.exercise import Exercise
-from src.payoff import Payoff, PayoffContext
+from src.payoff import Payoff, PayoffContext, Direction
 
 
 @dataclasses.dataclass(frozen = True, slots = True)
 class Option(ABC):
+    
     strike: float
     exercise: Exercise
     payoff: Payoff
@@ -14,3 +15,7 @@ class Option(ABC):
     @abstractmethod
     def payoff_value(self, context: PayoffContext) -> float: 
         return self.payoff.value(self.strike, context)
+    
+    @property
+    def direction(self) -> Direction:
+        return self.payoff.direction
