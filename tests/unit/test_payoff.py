94df8fd0fa8a2b790984fb7fa8c99bc6687d4ba6
Victor Sanchez <thespielmaster@MacBook-Pro-de-Victor.local>
2025-10-13 23:18:00 +0200

    test value call vanilla

--- tests/unit/test_payoff.py
+++ tests/unit/test_payoff.py
@@ -2,7 +2,7 @@ import unittest
 import sys
 
 sys.path.append('src')
-from src.payoff import PayoffFactory, Direction, PayoffType, VanillaPayoff
+from src.payoff import PayoffFactory, Direction, PayoffType, VanillaPayoff, PayoffContext
 
 class TestPayoffFactory(unittest.TestCase):
 
@@ -15,6 +15,23 @@ class TestPayoffFactory(unittest.TestCase):
         direction = Direction.CALL
         payoff = self.factory.create(PayoffType.VANILLA, direction)
         self.assertTrue(isinstance(payoff, VanillaPayoff))
+
+class TestPayoffVanilla(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.factory = PayoffFactory()
+        cls.payoff_context = PayoffContext(100.0, None)
+
+    def test_value_call_vanilla(self):
+        
+        direction = Direction.CALL
+        payoff = self.factory.create(PayoffType.VANILLA, direction)
+
+        self.assertEqual(payoff.value(strike=100.0, ctx = self.payoff_context), 0)
+        self.assertEqual(payoff.value(strike=101.0, ctx = self.payoff_context), 0)
+        self.assertEqual(payoff.value(strike=99.0, ctx = self.payoff_context), 1.0)
+
 
 if __name__ == '__main__':
     unittest.main()
