723454c9d90bf4d631fa559f82e5279f634c2423
Victor Sanchez <thespielmaster@MacBook-Pro-de-Victor.local>
2025-10-14 13:30:56 +0200

    fix greeks vanilla call

--- src/pricers/black_scholes.py
+++ src/pricers/black_scholes.py
@@ -8,7 +8,7 @@ from src.payoff import VanillaPayoff, Direction, PayoffContext
 from src.pricers.base import Pricer
 from src.pricers.types import Market, Greeks
 from src.option import Option
-from src.pricers.time_utils import year_fraction
+from src.pricers.time_utils import year_fraction, basis_mapping
 from src.pricers.factory import PricerFactory, PricerType
 
 @dataclass(frozen = True, slots = True)
@@ -59,9 +59,9 @@ class BlackScholesPricer(Pricer):
         
         sig_sqrt_t = params.sigma * np.sqrt(params.tau)
 
-        d1 = (np.log(params.S / params.K) + 
-              (params.r - params.q + 0.5 * params.sigma * params.sigma) 
-              * params.tau) / sig_sqrt_t
+        d1 = ( np.log(params.S / params.K)
+              + (params.r - params.q + 0.5 * params.sigma**2) * params.tau
+              ) / sig_sqrt_t 
         
         d2 = d1 - sig_sqrt_t
         
@@ -93,28 +93,35 @@ class BlackScholesPricer(Pricer):
         
     def greeks(self, option: Option, market: Market) -> Greeks:
 
-        S, K, r, q, sigma, tau, is_call = self.get_bs_inputs(option, market)
+        bs_params = self.get_bs_inputs(option, market)
 
-        if tau == 0.0 or sigma == 0.0:
+        if bs_params.tau == 0.0 or bs_params.sigma == 0.0:
             return Greeks(delta = None, gamma = None, vega = None, theta = None, rho = None)
         
-        sig_sqrt_t, d1, d2, disc_q, disc_r = self.compute_bs_quantities(S, K, tau, r, q, sigma)
+        bs_qtys = self.compute_bs_quantities(bs_params)
         
-        if is_call:
-            delta = disc_q * norm.cdf(d1)
-            theta = (-S * disc_q * norm.pdf(d1) * sigma / (2.0 * np.sqrt(tau))
-                     - r * K * disc_r * norm.cdf(d2)
-                     + q * S * disc_q * norm.cdf(d1))
-            rho   =  K * tau * disc_r * norm.cdf(d2)
-        else:
-            delta = disc_q * (norm.cdf(d1) - 1.0)
-            theta = (-S * disc_q * norm.pdf(d1) * sigma / (2.0 * np.sqrt(tau))
-                     + r * K * disc_r * norm.cdf(d2)
-                     - q * S * disc_q * norm.cdf(d1))
-            rho   = -K * tau * disc_r * norm.cdf(d2)
+        if bs_params.is_call:
 
-        gamma = (disc_q * norm.pdf(d1)) / (S * sig_sqrt_t)
-        vega  = S * disc_q * norm.cdf(d1) * np.sqrt(tau)
+            delta = bs_qtys.disc_q * norm.cdf(bs_qtys.d1)
+            
+            theta = ( -(bs_params.S * bs_params.sigma 
+                        * bs_qtys.disc_q * norm.pdf(bs_qtys.d1)) / (2 * np.sqrt(bs_params.tau))
+                        - bs_params.r * bs_params.K * bs_qtys.disc_r * norm.cdf(bs_qtys.d2)
+                        + bs_params.q * bs_params.S * bs_qtys.disc_q * norm.cdf(bs_qtys.d1)
+                     ) / basis_mapping[market.basis]
+            
+            rho   =  bs_params.K * bs_params.tau * bs_qtys.disc_r * norm.cdf(bs_qtys.d2) / 100
+        
+        else:
+            
+            delta = bs_qtys.disc_q * (norm.cdf(bs_qtys.d1) - 1.0)
+            
+            theta = None
+            
+            rho = -bs_params.K * bs_params.tau * bs_qtys.disc_r * norm.cdf(bs_qtys.d2)
+
+        gamma = (bs_qtys.disc_q * norm.pdf(bs_qtys.d1)) / (bs_params.S * bs_qtys.sig_sqrt_t)
+        vega  = bs_qtys.disc_q * bs_params.S * norm.pdf(bs_qtys.d1) * np.sqrt(bs_params.tau) / 100
 
         return Greeks(delta, gamma, vega, theta, rho)
 
